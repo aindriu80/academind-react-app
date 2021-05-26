3eb0bd30a4e479d53d16d57b3e29931675212294
Aindriú Mac Giolla Eoin <aindriu@pop-os.localdomain>
2021-05-26 10:58:37 +0100

    Outputting Dynamic Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 4, 26)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = '296.34'
   return (
     <div className="expense-item">
-      <div>25ú Bealtaine</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">€296.34</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">€{expenseAmount}</div>
       </div>
     </div>
   )
