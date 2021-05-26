3d6427040fce3e0031290d169ed0b07f19dff9af
Aindriú Mac Giolla Eoin <aindriu@gmail.com>
2021-05-26 16:42:40 +0100

    Composition - children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('ga-ie', { month: 'long' })
-  const year = props.date.getFullYear()
-  const day = props.date.toLocaleString('ga-ie', { day: '2-digit' })
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 export default ExpenseItem
