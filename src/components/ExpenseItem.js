716b1933b8b222dc4119ab35910e6c01373ba993
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-05-27 07:46:52 +0100

    git address

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,6 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('ga-ie', { month: 'long' })
-  const year = props.date.getFullYear()
-  const day = props.date.toLocaleString('ga-ie', { day: '2-digit' })
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
