44ab297b0fd064c3679fade0ee036504775c2741
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-05-30 10:41:50 +0100

    Child to Parent Component Communications

--- src/App.js
+++ src/App.js
@@ -23,10 +23,15 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ]
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
   return (
     <div>
       <h2>Academind React App </h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   )
