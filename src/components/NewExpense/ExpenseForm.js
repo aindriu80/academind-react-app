b48287becddbbdab96ac0241b0598de4bb466503
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-05-31 15:45:32 +0100

    Conditional Content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -56,7 +56,7 @@ const ExpenseForm = (props) => {
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controlds">
+      <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input
@@ -89,6 +89,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
